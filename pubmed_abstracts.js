cc050c7a0efaf835c0f5ac8c5d05b3bc0fee9a38
Liam Appelbe <liamappelbe@gmail.com>
2021-10-24 15:27:01 -0700

    web: More updates to pubmed API test page

--- pubmed_abstracts.js
+++ pubmed_abstracts.js
@@ -1,18 +1,42 @@
 (function() {
-async function asyncTextRequest(url, retries = 3) {
-  let error = null;
-  for (let i = 0; i < retries; ++i) {
+const kBaseUrl = 'https://eutils.ncbi.nlm.nih.gov/entrez/eutils';
+const kApis = {
+  esummary: 'esummary.fcgi?db=pmc&retmode=json',
+  efetch: 'efetch.fcgi?db=pubmed&retmode=xml',
+};
+const kRetries = 3;
+const kLink = 'https://pubmed.ncbi.nlm.nih.gov/';
+
+async function asyncPubMedRequest(api, id) {
+  const url = `${kBaseUrl}/${api}&id=${id}&${kPubMedApiSuffix}`;
+  let errorCode = null;
+  let errorResponse = null;
+  let bail = false;
+  for (let i = 0; i < kRetries; ++i) {
     try {
       return await new Promise((resolve, reject) => {
-        const request = new XMLHttpRequest();
-        request.type = 'text';
-        request.onload = () => resolve(request.responseText);
-        request.onerror = reject;
-        request.open('GET', url, true);
-        request.send();
+        const r = new XMLHttpRequest();
+        r.type = 'text';
+        r.onload = () => {
+          if (r.status == 200) {
+            resolve(r.responseText);
+          } else {
+            errorResponse = r.responseText;
+            errorCode = r.status;
+            if (errorCode == 400) {
+              // Special case 400 errors, because it means that the id was bad,
+              // so don't bother retrying.
+              bail = true;
+            }
+            reject();
+          }
+        };
+        r.onerror = reject;
+        r.open('GET', url, true);
+        r.send();
       });
     } catch (e) {
-      error = e;
+      if (bail) break;
       const t = 1 << i;
       console.log(`Retrying in ${t} seconds...`, url);
       await new Promise((resolve, reject) => {
@@ -20,7 +44,8 @@ async function asyncTextRequest(url, retries = 3) {
       });
     }
   }
-  console.error('Request failed: ', url, error);
+  console.error('Request failed: ', url, errorCode, errorResponse);
+  throw errorCode == 400 ? 'Bad PMID (A)' : 'Network Error';
 }
 
 class Xml {
@@ -62,16 +87,6 @@ class Xml {
   }
 }
 
-const kBaseUrl = 'https://eutils.ncbi.nlm.nih.gov/entrez/eutils';
-const kApis = {
-  esummary: 'esummary.fcgi?db=pmc&retmode=json',
-  efetch: 'efetch.fcgi?db=pubmed&retmode=xml',
-};
-
-async function asyncPubMedRequest(api, id) {
-  return asyncTextRequest(`${kBaseUrl}/${api}&id=${id}&${kPubMedApiSuffix}`);
-}
-
 function cleanText(text, end = null) {
   if (text == null) text = '';
   text = text.trim().replaceAll(/\s+/g, ' ');
@@ -87,27 +102,58 @@ function emptyDiv(n) {
   while (n.hasChildNodes()) n.removeChild(n.lastChild);
 }
 
-function newDiv(parent, classes = [], text = null) {
-  const n = document.createElement('div');
+function newElement(type, parent, classes = [], text = null) {
+  const n = document.createElement(type);
   if (text != null) n.innerText = text;
   for (const cls of classes) n.classList.add(cls);
   parent.appendChild(n);
   return n;
 }
 
+function newDiv(parent, classes = [], text = null) {
+  return newElement('div', parent, classes, text);
+}
+
+function newLink(parent, classes = [], url = null, text = null) {
+  const n = newElement('a', parent, classes, text);
+  if (url != null) n.href = url;
+  return n;
+}
+
 class PubMedImpl {
   constructor(node) {
     this.node = node;
-    this.pmid = this.node.getAttribute('pmid');
+    this.pmid = parseInt(this.node.getAttribute('pmid'));
     this.title = null;
     this.abstract = null;
     this.citation = null;
     window.setTimeout(async () => {
-      this._fill(Xml.parse(await asyncPubMedRequest(kApis.efetch, this.pmid)));
+      if (isNaN(this.pmid)) {
+        this._fillError('Bad PMID (B)');
+        return;
+      } else {
+        let data = null;
+        try {
+          data = Xml.parse(await asyncPubMedRequest(kApis.efetch, this.pmid));
+        } catch (e) {
+          this._fillError(e);
+          return;
+        }
+        this._fill(data);
+      }
     }, 0);
+  }
+  _fillError(error) {
+    emptyDiv(this.node);
+    this.node.classList.add('error');
+    this.node.innerText = error + ': ' + this.node.getAttribute('pmid');
   }
   _fill(data) {
     const mc = data?.one('PubmedArticle')?.one('MedlineCitation');
+    if (mc == null) {
+      this._fillError('Bad PMID (C)');
+      return;
+    }
     const article = mc?.one('Article');
     this.title = cleanText(article?.one('ArticleTitle')?.text, '.');
     this.abstract = article?.one('Abstract')?.all('AbstractText')?.map(x => {
@@ -115,8 +161,8 @@ class PubMedImpl {
     }) ??
         [];
     let authors = article?.one('AuthorList')?.all('Author')?.map(x => {
-      const lastName = cleanText(x?.one('LastName').text);
-      const initials = cleanText(x?.one('Initials').text);
+      const lastName = cleanText(x?.one('LastName')?.text);
+      const initials = cleanText(x?.one('Initials')?.text);
       return `${lastName} ${initials}`;
     }) ??
         [];
@@ -127,34 +173,39 @@ class PubMedImpl {
     }
     const authorText = authors.join(', ');
     const journal = article?.one('Journal');
-    const isoAbbr = cleanText(journal?.one('ISOAbbreviation').text, '.');
+    const isoAbbr = cleanText(journal?.one('ISOAbbreviation')?.text, '.');
     const journalIssue = journal?.one('JournalIssue');
-    const volume = cleanText(journalIssue?.one('Volume').text);
-    const issue = cleanText(journalIssue?.one('Issue').text);
+    const volume = cleanText(journalIssue?.one('Volume')?.text);
+    const issue = cleanText(journalIssue?.one('Issue')?.text);
     const pubDate = journalIssue?.one('PubDate');
-    const pubYear = cleanText(pubDate?.one('Year').text);
-    const pubMonth = cleanText(pubDate?.one('Month').text);
-    const page = cleanText(article?.one('Pagination')?.one('MedlinePgn').text);
+    const pubYear = cleanText(pubDate?.one('Year')?.text);
+    const pubMonth = cleanText(pubDate?.one('Month')?.text);
+    const page = cleanText(article?.one('Pagination')?.one('MedlinePgn')?.text);
     this.cite = `${authorText}. ${this.title} ${isoAbbr} ${pubYear} ` +
         `${pubMonth};${volume}(${issue}):${page}`;
     emptyDiv(this.node);
     this.node.classList.add('loaded');
-    newDiv(this.node, ['pub-med-citation'], this.cite)
-        .addEventListener('mouseover', () => this._click());
-    const kCopy = String.fromCodePoint(0x0001F5D0);
-    const btn = newDiv(this.node, ['pub-med-copy'], kCopy)
-                    .addEventListener('click', () => this._copy());
-    btn.title = 'Copy';
+    newDiv(this.node, ['pub-med-article'], `${this.title} ${authorText}`)
+        .addEventListener('mouseover', () => this._showAbstract());
+    const btn = newDiv(this.node, ['pub-med-copy'], '[]');
+    btn.addEventListener('click', () => this._copy());
+    btn.title = 'Copy citation';
   }
-  _click() {
+  _showAbstract() {
     const pma = document.getElementById('pub-med-abstract');
     if (pma == null) return;
     emptyDiv(pma);
-    newDiv(pma, ['pub-med-abstract-title'], this.title);
-    for (const [label, text] of this.abstract) {
+    newLink(pma, ['pub-med-abstract-title'], kLink + this.pmid, this.title);
+    newDiv(pma, ['pub-med-abstract-citation'], this.cite);
+    if (this.abstract.length == 0) {
       const part = newDiv(pma, ['pub-med-abstract-part']);
-      if (label.length > 0) newDiv(part, ['pub-med-abstract-label'], label);
-      newDiv(part, ['pub-med-abstract-text'], text);
+      newDiv(part, ['pub-med-abstract-text'], 'Abstract not available.');
+    } else {
+      for (const [label, text] of this.abstract) {
+        const part = newDiv(pma, ['pub-med-abstract-part']);
+        if (label.length > 0) newDiv(part, ['pub-med-abstract-label'], label);
+        newDiv(part, ['pub-med-abstract-text'], text);
+      }
     }
   }
   _copy() {
