965763914786c3ae83376c0a03bcd1c6c4b3ec41
Liam Appelbe <liamappelbe@gmail.com>
2022-06-20 20:35:24 -0700

    web: Slide show

---
+++ slide_show.js
@@ -0,0 +1,106 @@
+(function() {
+function newElement(type, parent, classes = [], text = null) {
+  const n = document.createElement(type);
+  if (text != null) n.innerText = text;
+  for (const cls of classes) n.classList.add(cls);
+  if (parent != null) parent.appendChild(n);
+  return n;
+}
+
+function newDiv(parent, classes = [], text = null) {
+  return newElement('div', parent, classes, text);
+}
+
+function newBtn(parent, classes = [], text = null, onclick = null) {
+  const btn = newDiv(parent, classes, text);
+  if (onclick != null) btn.addEventListener('click', onclick);
+  return btn;
+}
+
+function emptyDiv(n) {
+  while (n.hasChildNodes()) n.removeChild(n.lastChild);
+}
+
+class Slide {
+  constructor(wrap, dot) {
+    this.wrap = wrap;
+    this.dot = dot;
+  }
+}
+
+class SlideShowImpl {
+  constructor(node) {
+    this.node = node;
+    this.index = 0;
+    this.slides = [];
+    this.lowButtons = (node.getAttribute('button-layout') == 'low');
+
+    // Copy the children and remove them.
+    const ch = [];
+    for (const n of node.children) {
+      ch.push(n);
+    }
+    emptyDiv(node);
+
+    // Layout elements.
+    const slideRow = newDiv(node, ['slide-show-slide-row']);
+    const dotRow = newDiv(node, ['slide-show-dot-row']);
+    const btnWrap = this.lowButtons ? dotRow : slideRow;
+
+    // Previous button.
+    newBtn(btnWrap, ['slide-show-button'], '<', () => this.prevSlide());
+
+    // Wrap the children in some divs and re-add them.
+    const slideWrap = newDiv(slideRow, ['slide-show-slides']);
+    for (let i = 0; i < ch.length; ++i) {
+      const wrap = newDiv(slideWrap, ['slide-show-slide-wrap']);
+      wrap.appendChild(ch[i]);
+
+      // The progress dot.
+      const dot =
+          newBtn(dotRow, ['slide-show-dot'], null, () => this.setSlide(i));
+
+      this.slides.push(new Slide(wrap, dot));
+    }
+
+    // Next button.
+    newBtn(btnWrap, ['slide-show-button'], '>', () => this.nextSlide());
+
+    this.setSlide(0);
+
+    window.setTimeout(() => node.classList.add('loaded'), 0);
+  }
+
+  setSlide(index) {
+    this.index = index;
+    for (let i = 0; i < this.slides.length; ++i) {
+      const slide = this.slides[i];
+      if (i == index) {
+        slide.wrap.style.display = '';
+        slide.dot.classList.add('active');
+      } else {
+        slide.wrap.style.display = 'none';
+        slide.dot.classList.remove('active');
+      }
+    }
+  }
+
+  nextSlide() {
+    this.setSlide((this.index + 1) % this.slides.length);
+  }
+
+  prevSlide() {
+    this.setSlide((this.index + this.slides.length - 1) % this.slides.length);
+  }
+}
+
+class SlideShow extends HTMLElement {
+  constructor() {
+    super();
+    this.impl = new SlideShowImpl(this);
+  }
+}
+
+window.addEventListener(
+    'load', () => customElements.define('slide-show', SlideShow));
+})();
